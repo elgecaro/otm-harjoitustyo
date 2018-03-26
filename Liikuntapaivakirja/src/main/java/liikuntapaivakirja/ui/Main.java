eca375c88cfc8d88a8fc9f3682520eb248081474
Caroline <elgertcaroline@gmail.com>
2018-03-26 11:54:50 +0300

    tietokanta luotu

--- Liikuntapaivakirja/src/main/java/liikuntapaivakirja/ui/Main.java
+++ Liikuntapaivakirja/src/main/java/liikuntapaivakirja/ui/Main.java
@@ -1,14 +1,24 @@
 
 package liikuntapaivakirja.ui;
 
+import java.sql.*;
 
 public class Main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+        
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:tietokanta.db");
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT 1");
+        if(resultSet.next()) {
+            System.out.println("Hei tietokantamaailma!");
+        } else {
+            System.out.println("Yhteyden muodostaminen epäonnistui.");
+        }
     }
     
 }
