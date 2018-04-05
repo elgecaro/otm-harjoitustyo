6193a2520aceb3eeef78e71a3e931a164e259292
Caroline <elgertcaroline@gmail.com>
2018-04-05 15:14:09 +0300

    pohjat testeille luotu

---
+++ Liikuntapaivakirja/src/test/java/DiaryTest.java
@@ -0,0 +1,51 @@
+
+import liikuntapaivakirja.domain.Diary;
+import liikuntapaivakirja.domain.User;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class DiaryTest {
+    Diary diary;
+    User user;
+    
+    @Before
+    public void setUp() {
+        user = new User("Testikayttaja", "testisalasana");
+    }
+    
+    @Test
+    public void newDiaryEntryCorrectInformation() {
+        diary = new Diary(user, 1, 2, 3);
+        assertEquals("Testikayttaja", diary.getUsername());
+        assertEquals(1, diary.getHour(), 0);
+        assertEquals(2, diary.getDay());
+        assertEquals(3, diary.getWeek());
+    }
+    
+    @Test
+    public void newDiaryEntryCorrectContent() {
+        diary = new Diary(user, 1, 2, 3, "olin juoksemassa");
+        assertEquals("olin juoksemassa", diary.getContent());
+    }
+    
+    public DiaryTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+}
---
+++ Liikuntapaivakirja/src/test/java/UserTest.java
@@ -0,0 +1,47 @@
+
+import liikuntapaivakirja.domain.User;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class UserTest {
+    
+    User user;
+    
+    @Before
+    public void setUp() {
+        user = new User("Testikayttaja", "testisalasana");
+    }
+    
+    @Test
+    public void createdUserExist() {
+        assertTrue(user!=null);      
+    }
+    
+    @Test
+    public void usernameCorrect() {
+        assertEquals("Testikayttaja", user.getUsername());
+    }
+    
+    @Test
+    public void passwordCorrect() {
+        assertEquals("testisalasana", user.getPassword());
+    }      
+    
+    @Test
+    public void equalWhenSameUsername() {
+        User user2 = new User("Testikayttaja", "testisalasana");
+        assertTrue(user.getUsername().equals(user2.getUsername()));
+    }
+    
+    @Test
+    public void notEqualWhenDifferentUsername() {
+        User user2 = new User("Testikayttaja2", "testisalasana");
+        assertFalse(user.getUsername().equals(user2.getUsername()));
+    } 
+
+}
